957f5dda7d7a632a1f930359eefb741bc864728e
Le Hieu <hieuld@edsolabs.com>
2021-06-13 10:59:53 +0700

    test api

--- routers/size_tryon.py
+++ routers/size_tryon.py
@@ -1,7 +1,7 @@
 from starlette.responses import JSONResponse
 from pydantic import BaseModel
-import cv2
-import base64
+import matplotlib.pyplot as plt
+import shutil
 from fastapi.encoders import jsonable_encoder
 from fastapi import APIRouter
 from fastapi import Request
@@ -17,19 +17,22 @@ router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 client = Client()
 
-dict_category = {
-    "top":["long sleeve dress","short sleeve dress","short sleeve top","long sleeve top","vest","long sleeve outwear"],
-    "bottom":["trousers","skirt"]
-}
+src_result = "static/public/anh-tach-nen/"
+
+data = {
+    "id_ao": None,
+    "category_ao": None,
+    "id_quan": None,
+    "category_quan": None,
+    "body": 4985
+    }
+
+cache_DRESS = []
+cache_TOP = []
+cache_PANTS = []
+cache_SKIRT = []
 
 
-
-def resize_image(img,scale_percent):
-    width = int(img.shape[1]*(scale_percent/100))
-    height = int(img.shape[0]*(scale_percent/100))
-    dim = (width,height)
-    img = cv2.resize(img,dim,interpolation=cv2.INTER_AREA)
-    return img
 
 def convert_to_file(result_image):
     pass
@@ -39,7 +42,7 @@ def convert_to_file(result_image):
 @router.get("/products", response_class=HTMLResponse)
 async def api_fitsize(request: Request):
     """
-    Recommend size with each category
+    Recommend size with each category with input size charts and body measurement
     """
     return templates.TemplateResponse("inner-page-fix.html",{"request":request})
 
@@ -52,46 +55,56 @@ async def api_findsize(request: Request):
 
 #TRY ON
 @router.get("/tryon", response_class=HTMLResponse)
-async def api_tryon(request: Request):
+async def tryon_page(request: Request):
     return templates.TemplateResponse("tryon.html",{"request":request})
 
 
-data = {
-    "id_ao": None,
-    "category_ao": None,
-    "id_quan": None,
-    "category_quan": None,
-    "body": 4985
-    }
 
 @router.get("/result")
-async def api_get_result(iid:str, category:str, request: Request):
+async def api_get_result_tryon(iid:str, category:str, request: Request):
     """
     with each certain pants or shirt, proceed to try on the mannequin and return, display image to UI 
     """
-    
-    if category == "long sleeve dress" or category == "short sleeve dress":
-        data["id_ao"] = iid
-        data["category_ao"] = category
-        data["id_quan"] = None
-        data["category_quan"] = None
+    category = category.split("-")
+    cat = category[0]
+    value = category[1].replace(" ","_")
 
-    if category == "short sleeve top" or category == "long sleeve top" or category == "vest" or category == "long sleeve outwear":
+    if cat == "DRESS":
         data["id_ao"] = iid
-        data["category_ao"] = category
-
-    if category == "trousers" or category == "skirt":
+        data["category_ao"] = value
         data["id_quan"] = iid
-        data["category_quan"] = category
+        data["category_quan"] = value
 
-    #url = "http://192.168.50.69:5849/{}/{}/{}/{}/{}".format(data["id_ao"],data["category_ao"],data["id_quan"],data["category_quan"],data["body"])
+    elif cat == "TOP":
+        data["id_ao"] = iid
+        data["category_ao"] = value
+        if cache_PANTS == [] and cache_SKIRT == []:
+            #image = plt.imread(src_result + "Top/IMG_{}.png".format(iid))
+            shutil.copy(src_result + "Top/IMG_{}.png".format(iid),src_result + "result.png") 
+
+    elif cat == "PANTS":
+        data["id_quan"] = iid
+        data["category_quan"] = value
+        if cache_PANTS == []:
+            shutil.copy(src_result + "Pants/IMG_{}.png".format(iid),src_result + "result.png")
+        cache_PANTS.append(data["id_quan"])
+
+    elif cat == "SKIRT":
+        data["id_quan"] = iid
+        data["category_quan"] = value
+        if cache_SKIRT == []:
+            shutil.copy(src_result + "Skirt/IMG_{}.png".format(iid),src_result + "result.png")
+        cache_SKIRT.append(data["id_quan"])
+
+    url = "http://192.168.50.69:5849/{}/{}/{}/{}/{}".format(data["id_ao"],data["category_ao"],data["id_quan"],data["category_quan"],data["body"])
+    
     response = requests.get(url="http://192.168.50.69:5849/4990/trousers/5013/long_sleeve_top/4985")
     result = response.content
     image = base64.b64decode(result.content)
-    filename = '/home/hieuld/project_RS_VFR/demo_web_VFR/static/public/tryon-image/anh-tach-nen/image.png'
+    filename = 'static/public/anh-tach-nen//image.png'
     
     with open(filename, 'wb') as f:
-        f.write(image)
+         f.write(image)
 
     message = "DONE!"
     return templates.TemplateResponse("tryon.html",{"request":request,"message":message})
